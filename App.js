e526e6940574ba4be65ef61ea370354b4142e4ab
Zinedine7 <57072017+Zinedine7@users.noreply.github.com>
2020-05-24 19:36:47 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import Navigation from "./components/navbar.component";
+import Login from "./components/Login.component";
+import SignUp from "./components/SignUp.component";
+import Courses from "./components/Courses.component";
+import UserCourses from "./components/UserCourses.component";
+
+const containerStyle = {
+    'maxWidth': '100%',
+    'paddingLeft': '0px',
+    'paddingRight': '0px'
+};
+
+
+function App() {
+  return (
+      <Router>
+          <div className="container" style={containerStyle}>
+            <Navigation />
+            <br/>
+            <Route path="/login" component={Login}/>
+            <Route path="/Signup" component={SignUp}/>
+            <Route path="/Courses" component={Courses}/>
+            <Route path="/UserCourses" component={UserCourses}/>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render( <App />, document.getElementById('root')) ;
+var nav = document.querySelector(".navigation");
+var logo = document.querySelector(".logoTrans");
+var upButtons = document.querySelector(".upButtons");
+var downButtons = document.querySelector(".downButtons");
+var logOut = document.querySelector("#LogOut");
+var user = document.querySelector(".user");
+var switch1 = document.querySelector(".switch");
+var iusTrans = document.querySelector(".iusTrans");
+var menu = document.querySelector(".menu");
+var navLinks = document.querySelector(".links");
+var flag = true;
+var cards = document.getElementsByClassName("card");
+
+if (window.innerWidth <= 768) {
+  nav.style.display = "none";
+  for (var i = 0; i < cards.length; i++) {
+    if (cards.item(i).innerHTML == "") {
+      cards.item(i).remove();
+    }
+  }
+}
+
+menu.addEventListener("click", function () {
+  if (flag) {
+    nav.style.display = "";
+    flag = false;
+  } else {
+    nav.style.display = "none";
+    flag = true;
+  }
+});
+
+(function ($) {
+  "use strict";
+
+  /*==================================================================
+  [ Focus input ]*/
+  $(".input100").each(function () {
+    $(this).on("blur", function () {
+      if ($(this).val().trim() != "") {
+        $(this).addClass("has-val");
+      } else {
+        $(this).removeClass("has-val");
+      }
+    });
+  });
+
+  /*==================================================================
+  [ Validate ]*/
+  var input = $(".validate-input .input100");
+
+  $(".validate-form").on("submit", function () {
+    var check = true;
+
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+
+    return check;
+  });
+
+  $(".validate-form .input100").each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
+    });
+  });
+
+  function validate(input) {
+    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) == null
+      ) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == "") {
+        return false;
+      }
+    }
+  }
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass("alert-validate");
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass("alert-validate");
+  }
+
+  /*==================================================================
+  [ Show pass ]*/
+  var showPass = 0;
+  $(".btn-show-pass").on("click", function () {
+    if (showPass == 0) {
+      $(this).next("input").attr("type", "text");
+      $(this).addClass("active");
+      showPass = 1;
+    } else {
+      $(this).next("input").attr("type", "password");
+      $(this).removeClass("active");
+      showPass = 0;
+    }
+  });
+})(jQuery);
+
